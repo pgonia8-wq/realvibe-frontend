41c4652e0e26be4df76f15e3fbc7c810f23c8cf7
pgonia8-wq <pgonia8@gmail.com>
2026-02-18 08:49:41 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Switch, Route } from "wouter";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/auth" component={AuthPage} />
+      <Route path="/" component={HomePage} />
+    </Switch>
+  );
+}
+
+export default App;
