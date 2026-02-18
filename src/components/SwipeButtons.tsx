4825433efc22451acd8d8b9e346abdf7ac0d41ba
pgonia8-wq <pgonia8@gmail.com>
2026-02-18 11:59:08 -0600

    Update SwipeButtons.tsx

--- src/components/SwipeButtons.tsx
+++ src/components/SwipeButtons.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 interface SwipeButtonsProps {
-  onAction: (type: string) => void;
+  onAction: (type: string, cost?: number) => void;
 }
 
 export default function SwipeButtons({ onAction }: SwipeButtonsProps) {
   return (
     <div className="flex flex-wrap justify-center mt-4 gap-2">
       <button className="btn-dislike" onClick={() => onAction("DISLIKE")}>Dislike</button>
-      <button className="btn-superlike" onClick={() => onAction("SUPERLIKE")}>Super Like 1 WLD</button>
+      <button className="btn-superlike" onClick={() => onAction("SUPERLIKE", 1)}>Super Like 1 WLD</button>
       <button className="btn-like" onClick={() => onAction("LIKE")}>Like</button>
-      <button className="btn-boost" onClick={() => onAction("BOOST")}>Boost 1 WLD</button>
-      <button className="btn-boost" onClick={() => onAction("GOLD")}>Gold 10 WLD</button>
-      <button className="btn-boost" onClick={() => onAction("PLATINUM")}>Platinum 25 WLD</button>
-      <button className="btn-boost" onClick={() => onAction("DIAMOND")}>Diamond 40 WLD</button>
+      <button className="btn-boost" onClick={() => onAction("BOOST", 1)}>Boost 1 WLD</button>
+      <button className="btn-gold" onClick={() => onAction("GOLD", 10)}>Gold 10 WLD</button>
+      <button className="btn-platinum" onClick={() => onAction("PLATINUM", 25)}>Platinum 25 WLD</button>
+      <button className="btn-diamond" onClick={() => onAction("DIAMOND", 40)}>Diamond 40 WLD</button>
     </div>
   );
-      }
+                                                                                      }
