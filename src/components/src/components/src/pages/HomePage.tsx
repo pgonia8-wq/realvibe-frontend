8ca24d39eebfbf0cb15cd1f4a2b4678e65a4536b
pgonia8-wq <pgonia8@gmail.com>
2026-02-18 08:57:59 -0600

    Create HomePage.tsx

---
+++ src/components/src/components/src/pages/HomePage.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SwipeCard from "../components/SwipeCard";
+import SwipeButtons from "../components/SwipeButtons";
+
+interface User {
+  id: number;
+  username: string;
+  bio: string;
+  photo_url: string;
+}
+
+export default function HomePage() {
+  const [profiles, setProfiles] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios.get("https://TU_BACKEND_URL/api/profiles", { headers: { "x-worldapp-id": 1 } })
+      .then(res => setProfiles(res.data.profiles))
+      .catch(err => console.log(err));
+  }, []);
+
+  const handleAction = (type: string) => {
+    if (profiles.length === 0) return;
+    const target = profiles[0];
+    axios.post("https://TU_BACKEND_URL/api/swipe", { targetId: target.id, actionType: type }, { headers: { "x-worldapp-id": 1 } })
+      .then(() => setProfiles(prev => prev.slice(1)))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-10">
+      {profiles.length > 0 ? <SwipeCard {...profiles[0]} /> : <p>No hay más perfiles.</p>}
+      <SwipeButtons onAction={handleAction} />
+    </div>
+  );
+}
