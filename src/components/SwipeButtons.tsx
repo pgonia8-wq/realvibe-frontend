ce2437322001e2c714307d1961785c6ef6cb0e3c
pgonia8-wq <pgonia8@gmail.com>
2026-02-18 10:33:25 -0600

    Create SwipeButtons.tsx

---
+++ src/components/SwipeButtons.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface SwipeButtonsProps {
+  onAction: (type: string) => void;
+}
+
+export default function SwipeButtons({ onAction }: SwipeButtonsProps) {
+  return (
+    <div className="flex flex-wrap justify-center mt-4 gap-2">
+      <button className="btn-dislike" onClick={() => onAction("DISLIKE")}>Dislike</button>
+      <button className="btn-superlike" onClick={() => onAction("SUPERLIKE")}>Super Like 1 WLD</button>
+      <button className="btn-like" onClick={() => onAction("LIKE")}>Like</button>
+      <button className="btn-boost" onClick={() => onAction("BOOST")}>Boost 1 WLD</button>
+      <button className="btn-boost" onClick={() => onAction("GOLD")}>Gold 10 WLD</button>
+      <button className="btn-boost" onClick={() => onAction("PLATINUM")}>Platinum 25 WLD</button>
+      <button className="btn-boost" onClick={() => onAction("DIAMOND")}>Diamond 40 WLD</button>
+    </div>
+  );
+      }
