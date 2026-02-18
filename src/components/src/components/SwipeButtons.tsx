eb64d7f60c641b6d3be732d77ac5bfc1ff52730a
pgonia8-wq <pgonia8@gmail.com>
2026-02-18 09:19:59 -0600

    SwipeButtons.tsx

--- src/components/src/components/SwipeButtons.tsx
+++ src/components/src/components/SwipeButtons.tsx
@@ -6,10 +6,14 @@ interface SwipeButtonsProps {
 
 export default function SwipeButtons({ onAction }: SwipeButtonsProps) {
   return (
-    <div className="flex justify-around mt-4">
+    <div className="flex flex-wrap justify-center mt-4 gap-2">
       <button className="btn-dislike" onClick={() => onAction("DISLIKE")}>Dislike</button>
-      <button className="btn-superlike" onClick={() => onAction("SUPERLIKE")}>Super Like</button>
+      <button className="btn-superlike" onClick={() => onAction("SUPERLIKE")}>Super Like 1 WLD</button>
       <button className="btn-like" onClick={() => onAction("LIKE")}>Like</button>
+      <button className="btn-boost" onClick={() => onAction("BOOST")}>Boost 1 WLD</button>
+      <button className="btn-boost" onClick={() => onAction("GOLD")}>Gold 10 WLD</button>
+      <button className="btn-boost" onClick={() => onAction("PLATINUM")}>Platinum 25 WLD</button>
+      <button className="btn-boost" onClick={() => onAction("DIAMOND")}>Diamond 40 WLD</button>
     </div>
   );
-}
+      }
