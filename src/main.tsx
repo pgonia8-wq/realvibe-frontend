0326dcf13ea59637f645634360e39961de897ea4
pgonia8-wq <pgonia8@gmail.com>
2026-02-19 02:26:28 -0600

    agregar MiniKitProvider wrapper para inicializar SDK correctamente

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-// src/main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';  // ← si tu archivo App se llama App.tsx, déjalo así; si es App.js, cámbialo
+import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MiniKitProvider>
+      <App />
+    </MiniKitProvider>
   </React.StrictMode>
 );
